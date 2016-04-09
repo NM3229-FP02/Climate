c9ba28ca8af5ef699e06ae4fbbf699ad0e5e3594
Jenny Wen <jennywen27@gmail.com>
2016-04-09 16:25:44 +0800

    Added nav to each section, some changes to layout and rank graph

--- src/js/main.js
+++ src/js/main.js
@@ -7,18 +7,16 @@ $('.rank-country-container').on('click', function(){
     selfSubject = '.rank-container-temp';
   }
 
-  console.log(toggleSubject + selfSubject);
-
   if ($(this).hasClass('active')) {
     $(this).removeClass('active');
     $(toggleSubject + ' .rank-country-container').show();
   } else {
-    $('.rank-country-container').removeClass('active');
+    $('.rank-country-container').removeClass('active').removeClass('active-toggle');
     $(selfSubject + ' .rank-country-container').show();
     $(this).addClass('active');
     countryId = $(this).attr('id');
     $(toggleSubject + ' .rank-country-container').show().hide();
-    $(toggleSubject + ' .rank-country-container#' + countryId).show();
+    $(toggleSubject + ' .rank-country-container#' + countryId).show().addClass('active-toggle');
   }
 
 });
